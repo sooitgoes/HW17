774f49d78d11ae980a68a6699da9604dd53ce257
Mikhail Laty <laty.mischa@gmail.com>
2023-08-09 15:02:07 +0300

    Copy the bruteForce method from the original project.

--- HW17/Sources/ViewController.swift
+++ HW17/Sources/ViewController.swift
@@ -9,9 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - UI Elements
+
+
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+
+    // MARK: - Setup
+    private func setupHierarchy() {
+
+    }
+
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+
+        ])
+    }
+
+    func bruteForce(passwordToUnlock: String) {
+        let ALLOWED_CHARACTERS: [String] = String().printable.map { String($0) }
+        var password: String = ""
+
+        while password != passwordToUnlock {
+            password = generateBruteForce(password, fromArray: ALLOWED_CHARACTERS)
+            print(password)
+        }
+        print(password)
+    }
+
+    // MARK: - Action
+
 
 
 }
