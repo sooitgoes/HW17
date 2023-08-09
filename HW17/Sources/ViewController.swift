3b454749d2450f36af92c34e04e1b3f1124c20d3
Mikhail Laty <laty.mischa@gmail.com>
2023-08-09 16:54:14 +0300

    Add ui elements and configure their.

--- HW17/Sources/ViewController.swift
+++ HW17/Sources/ViewController.swift
@@ -10,20 +10,109 @@ import UIKit
 class ViewController: UIViewController {
 
     // MARK: - UI Elements
+    private lazy var passwordField: UITextField = {
+        let passwordField = UITextField()
+        passwordField.placeholder = "Password"
+        passwordField.textAlignment = .center
+        passwordField.font = .systemFont(ofSize: 15)
+        passwordField.layer.borderWidth = 0.5
+        passwordField.isSecureTextEntry = true
+        passwordField.layer.cornerRadius = 20
+        passwordField.translatesAutoresizingMaskIntoConstraints = false
+        return passwordField
+    }()
+
+    private lazy var activityIndicator: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView(style: .large)
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        return indicator
+    }()
+
+    private lazy var passwordLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Pass"
+        label.font = .systemFont(ofSize: 20, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private lazy var buttonForPasswordGeneration: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Generate a password ", for: .normal)
+        button.titleLabel?.numberOfLines = 0
+        button.titleLabel?.textAlignment = .center
+        button.titleLabel?.font = .systemFont(ofSize: 12, weight: .bold)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 20
+        button.backgroundColor = .systemGray
+        return button
+    }()
+
+    private lazy var backgroundColorButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Change background color", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 12, weight: .bold)
+        button.titleLabel?.numberOfLines = 0
+        button.titleLabel?.textAlignment = .center
+        button.setTitleColor(.white, for: .normal)
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 20
+        button.backgroundColor = .systemGray
+        return button
+    }()
+
+    private lazy var stackButtons: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.spacing = 25
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
 
 
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupHierarchy()
+        setupLayout()
+        view.backgroundColor = .systemBackground
+        activityIndicator.startAnimating()
     }
 
     // MARK: - Setup
     private func setupHierarchy() {
-
+        stackButtons.addArrangedSubview(backgroundColorButton)
+        stackButtons.addArrangedSubview(buttonForPasswordGeneration)
+        view.addSubview(activityIndicator)
+        view.addSubview(passwordField)
+        view.addSubview(passwordLabel)
+        view.addSubview(stackButtons)
     }
 
     private func setupLayout() {
         NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.topAnchor.constraint(equalTo: view.topAnchor, constant: 80),
+
+            passwordField.topAnchor.constraint(equalTo: activityIndicator.bottomAnchor, constant: 100),
+            passwordField.bottomAnchor.constraint(equalTo: passwordLabel.topAnchor, constant: -100),
+            passwordField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            passwordField.widthAnchor.constraint(equalToConstant: 300),
+            passwordField.heightAnchor.constraint(equalToConstant: 44),
+
+            passwordLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            passwordLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+
+            backgroundColorButton.heightAnchor.constraint(equalToConstant: 44),
+            backgroundColorButton.widthAnchor.constraint(equalToConstant: 150),
+
+            buttonForPasswordGeneration.heightAnchor.constraint(equalToConstant: 44),
+            buttonForPasswordGeneration.widthAnchor.constraint(equalToConstant: 150),
+
+            stackButtons.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackButtons.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -120)
+
 
         ])
     }
